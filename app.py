e31a8148787412de7a28362f10a31b46185b9f35
Your Name <your@example.com>
2026-02-12 12:14:53 +0530

    hello

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/love')
+def love():
+    return render_template('love.html')
+
+@app.route('/secret')
+def secret():
+    return render_template('secret.html')
+
+# 🔥 ADD THIS
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
